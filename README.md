e6998c5fa67f5aa28c26d53153b2dfb6547bb5b5
liuguanbang <salodyliu@gmail.com>
2017-10-01 22:34:24 +0800

    成功配置微信公众号服务器。与微信服务器通信成功

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-sd
+##Nodejs的运维
+
+* 查看node进程：ps -ef | grep node
+* 查看80端口占用： fuser -n tcp 80 
---
+++ config/index.js
@@ -0,0 +1,16 @@
+/**
+ * 功能描述: 配置文件
+ * @author: liuguanbang
+ * 2017/10/1
+ */
+
+const config = {
+  port: 80,
+  wechat: {
+    AppID: 'wxbb3a41e497d23f1c',
+    AppSecret: '01034d28e71aca3d05957b9abe13f016',
+    token: 'whosyourdaddy'
+  }
+};
+
+module.exports = config;
