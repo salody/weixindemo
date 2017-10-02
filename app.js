ea0bce5ed2ec775b08356ec6be01fe42ecfad839
liuguanbang <salodyliu@gmail.com>
2017-10-02 15:38:36 +0800

    添加Pm2

--- app.js
+++ app.js
@@ -5,28 +5,10 @@
  */
 
 const config = require('./config');
-const sha1 = require('sha1');
+const wechat = require('./middlewares/wechat');
 const koa = require('koa');
 const app = new koa();
 
-app.use(function *connect(next) {
-  let token = config.wechat.token;
-  let signature = this.query.signature;
-  let timestamp = this.query.timestamp;
-  let nonce = this.query.nonce;
-  let echostr = this.query.echostr;
-
-  // 加密/校验流程如下：
-  // 1.将token、timestamp、nonce三个参数进行字典序排序
-  let str = [token, timestamp, nonce].sort().join('');
-  // 2. 将三个参数字符串拼接成一个字符串进行sha1加密
-  let shaStr = sha1(str);
-  // 3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-  if (shaStr === signature) {
-    this.body = echostr + '';
-  } else {
-    this.body = '这里只接受微信后台的访问哦';
-  }
-});
+app.use(wechat(config.wechat));
 
 app.listen(config.port);
